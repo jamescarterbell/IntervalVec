5739bf64bff2e40b403db8438b027cbe4ad5445a
James Bell <t-jabell@microsoft.com>
2020-08-07 11:42:56 -0400

    More groundwork

--- src/rotations.rs
+++ src/rotations.rs
@@ -1,4 +1,5 @@
 use super::*;
+use std::mem;
 
 impl<T> IntervalNode<T>
     where T: PartialEq + Clone{
@@ -29,5 +30,35 @@ impl<T> IntervalNode<T>
         };
 
         left_height - right_height
+    }
+
+    /// Returns None if it has no right node.
+    /// If a right node returns None, this will deconstruct
+    /// said node and return it up.
+    fn right_most_mut(&mut self) -> Option<Box<IntervalNode<T>>>{
+        let r_node = match &mut self.right{
+            Some(node) => self.right_most_mut(),
+            None => return None,
+        };
+
+        match r_node{
+            Some(node) => return Some(node),
+            None => mem::replace(&mut self.right, None),
+        }
+    }
+
+    /// Returns None if it has no left node.
+    /// If a left node returns None, this will deconstruct
+    /// said node and return it up.
+    fn left_most_mut(&mut self) -> Option<Box<IntervalNode<T>>>{
+        let l_node = match &mut self.left{
+            Some(node) => self.left_most_mut(),
+            None => return None,
+        };
+
+        match l_node{
+            Some(node) => return Some(node),
+            None => mem::replace(&mut self.left, None),
+        }
     }
 }
